84cc6deb2af18c06f5890013b2b1687765ce7089
yoloClin <yoloclin@github.com>
2019-11-05 12:42:58 +1100

    Now adheres to env variables

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 sed "s/MPD_ICE_PASSWORD/$MPD_ICE_PASSWORD/g" /etc/icecast.xml.template | \
     sed "s/ICE_ADMIN_PASSWORD/$ICE_ADMIN_PASSWORD/g" > /etc/icecast.xml
 
+touch /etc/mime.types
+
 sudo -u icecast /usr/bin/icecast -c /etc/icecast.xml
